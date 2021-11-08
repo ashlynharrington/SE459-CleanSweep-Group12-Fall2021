983c2038e905345cea6752976c028f0a24147cef
Raj Patel <rajpatel.12213@gmail.com>
2021-11-08 13:48:02 -0600

    Added templates with the html pages

--- src/main/java/SE459/CleanSweep/controller/Register.java
+++ src/main/java/SE459/CleanSweep/controller/Register.java
@@ -1,7 +1,10 @@
 package SE459.CleanSweep.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Register {
@@ -11,8 +14,18 @@ public class Register {
     controller handles links
      */
 
+    /*
     @RequestMapping("/register")
     public String register() {
         return "Register";
+    }
+     */
+
+    @RequestMapping ("/register")
+    public ModelAndView register (Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register");
+        model.addAttribute("greeting","this is a custom message");
+        return modelAndView;
     }
 }
--- src/main/java/SE459/CleanSweep/model/User.java
+++ src/main/java/SE459/CleanSweep/model/User.java
@@ -1,12 +1,11 @@
 package SE459.CleanSweep.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-@Entity
+
+
 public class User {
 
-    @Id
+
     private Long cleanSweepId;
 
     private String firstName;
