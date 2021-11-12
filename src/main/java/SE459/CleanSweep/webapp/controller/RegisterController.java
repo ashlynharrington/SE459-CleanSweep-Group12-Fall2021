278483543a93b7fdcd49f13ba1ba5e251275c7ec
stephaniebasaraba <stephanie.basaraba@gmail.com>
2021-11-11 19:09:00 -0600

    recommit for web app

--- src/main/java/SE459/CleanSweep/webapp/controller/RegisterController.java
+++ src/main/java/SE459/CleanSweep/webapp/controller/RegisterController.java
@@ -1,14 +1,25 @@
 package SE459.CleanSweep.webapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import SE459.CleanSweep.webapp.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class register {
+@Controller
+public class RegisterController {
 
-    @RequestMapping("/register")
-    public String register() {
-        return "Register";
+    @GetMapping("/register")
+    public String getRegistration(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitRegistration(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        System.out.println(user);
+        return "welcome";
     }
 
 }
--- src/main/java/SE459/CleanSweep/webapp/model/User.java
+++ src/main/java/SE459/CleanSweep/webapp/model/User.java
@@ -1,2 +1,60 @@
-package SE459.CleanSweep.webapp.model;public class user {
+package SE459.CleanSweep.webapp.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String cleanSweepID;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCleanSweepID() {
+        return cleanSweepID;
+    }
+
+    public void setCleanSweepID(String cleanSweepID) {
+        this.cleanSweepID = cleanSweepID;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", cleanSweepID='" + cleanSweepID + '\'' +
+                '}';
+    }
 }
